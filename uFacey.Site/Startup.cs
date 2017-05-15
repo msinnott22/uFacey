64b33d840a24bbdabf78b14487a059c0a1e9436b
msinnott22 <msinnott21@gmail.com>
2017-05-15 23:30:17 +0100

    Added nuget package of uFacey. Need to reset admin password.

---
+++ uFacey.Site/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Security;
+using umbraco;
+using umbraco.BusinessLogic;
+using Umbraco.Core;
+using Umbraco.Core.Configuration;
+using Umbraco.Web.Security.Providers;
+
+namespace uFacey.Site
+{
+    public class Startup : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication,
+            ApplicationContext applicationContext)
+        {
+            var resetAdminPassword = WebConfigurationManager.AppSettings["ResetAdminPassword"];
+
+            if (resetAdminPassword == true.ToString())
+            {
+                var userService = applicationContext.Services.UserService;
+
+                var adminUser = userService.GetUserById(0);
+
+                if (adminUser != null)
+                {
+                    adminUser.IsLockedOut = false;
+                    adminUser.IsApproved = true;
+                    
+                    userService.Save(adminUser);
+
+                    //userService.SavePassword(adminUser, "Admin1234!");
+                }
+            }
+        }
+    }
+}
